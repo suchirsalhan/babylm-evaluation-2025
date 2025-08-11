4fd0e19e1d5dd592f5210a5cfd350eaa10d30a6b
Suchir Salhan <suchirsalhan@gmail.com>
2025-08-11 15:08:00 +0100

    Create coherence.sh

---
+++ coherence.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+models=(
+  "suchirsalhan/coh1em00"
+  "suchirsalhan/coh1em01"
+  "suchirsalhan/coh1em02"
+  "suchirsalhan/coh1em03"
+)
+
+tasks=(
+  "causal"
+  "mlm"
+  "mntp"
+)
+
+for model in "${models[@]}"
+do
+  for task in "${tasks[@]}"
+  do
+    echo "Running evaluation for $model on task $task"
+    ./eval_zero_shot.sh "$model" "$task"
+  done
+done
